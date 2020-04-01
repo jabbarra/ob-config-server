5b311b9867328ac9004f86a483b15e6f24186a5d
obarra <barra_omar@hotmail.com>
2020-04-01 20:14:07 -0300

    config-server

---
+++ src/test/java/com/obarra/obconfigserver/ObConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obarra.obconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
