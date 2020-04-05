53c685e8b9d780171216796f88938e381a4da327
obarra <barra_omar@hotmail.com>
2020-04-04 22:50:07 -0300

    config checkstyle

--- src/main/java/com/obarra/obconfigserver/ObConfigServerApplication.java
+++ src/main/java/com/obarra/obconfigserver/ObConfigServerApplication.java
@@ -8,8 +8,12 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 public class ObConfigServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ObConfigServerApplication.class, args);
-	}
+    /**
+     * Principal entry point of Spring Boot Applications.
+     * @param args receives values of executor
+     */
+    public static void main(final String[] args) {
+        SpringApplication.run(ObConfigServerApplication.class, args);
+    }
 
 }
